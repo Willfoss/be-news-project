d36157a1d277f476e92dc6d9fe4eb8f707d27ab8
Willfoss <118803774+Willfoss@users.noreply.github.com>
2024-07-15 15:02:11 +0100

    5 get /api/articles completed with testing. endpoint object existed already but had the wrong example - this was also updated

--- controllers/article-controller.js
+++ controllers/article-controller.js
@@ -1,10 +1,20 @@
-const { fetchArticleById } = require("../models/article-models");
+const { fetchArticleById, fetchArticles } = require("../models/article-models");
 
 exports.getArticleById = (request, response, next) => {
   const { articleId } = request.params;
   fetchArticleById(articleId)
     .then((article) => {
       return response.send({ article });
+    })
+    .catch((error) => {
+      next(error);
+    });
+};
+
+exports.getArticles = (request, response, next) => {
+  fetchArticles()
+    .then((articles) => {
+      return response.send({ articles });
     })
     .catch((error) => {
       next(error);
--- models/article-models.js
+++ models/article-models.js
@@ -14,3 +14,13 @@ exports.fetchArticleById = (id) => {
       return rows[0];
     });
 };
+
+exports.fetchArticles = () => {
+  const queryString = `SELECT articles.article_id, articles.author, articles.title, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id)::int AS comment_count FROM articles 
+  LEFT JOIN comments ON comments.article_id=articles.article_id
+  GROUP BY articles.article_id
+  ORDER BY created_at DESC`;
+  return db.query(queryString).then(({ rows }) => {
+    return rows;
+  });
+};
